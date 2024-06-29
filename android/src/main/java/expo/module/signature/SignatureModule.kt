b7492d65171809466f0af0aabca81209c709d696
Riccardo Pizzoni <INDAPP.info@gmail.com>
2024-06-29 15:54:16 +0200

    refactor: rename module to ExpoSignature

--- android/src/main/java/expo/module/signature/SignatureModule.kt
+++ android/src/main/java/expo/module/signature/SignatureModule.kt
@@ -11,7 +11,7 @@ class SignatureModule : Module() {
     // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
     // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
     // The module will be accessible from `requireNativeModule('SignatureModule')` in JavaScript.
-    Name("SignatureModule")
+    Name("ExpoSignature")
 
     // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
     Constants(
