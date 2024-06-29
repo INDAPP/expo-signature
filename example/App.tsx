bbea40e68aa45e4870781aa464577426b8ea7282
Riccardo Pizzoni <INDAPP.info@gmail.com>
2024-06-29 23:05:40 +0200

    feat: setup example app to generate key pair

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import * as SignatureModule from 'expo-signature';
+import { generateEllipticCurveKeys } from "expo-signature";
+import { PublicKey } from "expo-signature/SignatureModule.types";
+import { useCallback, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [publicKey, setPublicKey] = useState<PublicKey>();
+
+  const generateKeyPair = useCallback(async () => {
+    const publicKey = await generateEllipticCurveKeys("test_ecc_key");
+    setPublicKey(publicKey);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>{SignatureModule.hello()}</Text>
+      <Text>{JSON.stringify(publicKey, null, 2)}</Text>
+      <Button title="Generate Key Pair" onPress={generateKeyPair} />
     </View>
   );
 }
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
