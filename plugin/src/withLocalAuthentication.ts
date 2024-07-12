071ff6d08fc58fa5250ba6af5a6e3d24d4e6a366
Riccardo Pizzoni <INDAPP.info@gmail.com>
2024-07-12 22:55:48 +0200

    build: create expo prebuild plugin

---
+++ plugin/src/withLocalAuthentication.ts
@@ -0,0 +1,30 @@
+import {
+  AndroidConfig,
+  ConfigPlugin,
+  IOSConfig,
+  createRunOncePlugin,
+} from "expo/config-plugins";
+
+const pkg = require("../../package.json");
+const FACE_ID_USAGE = "Allow $(PRODUCT_NAME) to use Face ID";
+
+const withLocalAuthentication: ConfigPlugin<
+  { faceIDPermission?: string | false } | void
+> = (config, { faceIDPermission } = {}) => {
+  IOSConfig.Permissions.createPermissionsPlugin({
+    NSFaceIDUsageDescription: FACE_ID_USAGE,
+  })(config, {
+    NSFaceIDUsageDescription: faceIDPermission,
+  });
+
+  return AndroidConfig.Permissions.withPermissions(config, [
+    "android.permission.USE_BIOMETRIC",
+    "android.permission.USE_FINGERPRINT",
+  ]);
+};
+
+export default createRunOncePlugin(
+  withLocalAuthentication,
+  pkg.name,
+  pkg.version,
+);
