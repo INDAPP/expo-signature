4dfd888bcae7befa175a29885dff1af4a04bc7fc
Riccardo Pizzoni <INDAPP.info@gmail.com>
2024-08-07 22:18:49 +0200

    refactor: update methods and classes signature

--- android/src/main/java/expo/module/signature/models/KeySpec.kt
+++ android/src/main/java/expo/module/signature/models/KeySpec.kt
@@ -3,9 +3,9 @@ package expo.module.signature.models
 import expo.modules.kotlin.records.Field
 import expo.modules.kotlin.records.Record
 
-class KeySpec: Record {
-    @Field val algorithm: SignatureAlgorithm = SignatureAlgorithm.EC
-    @Field val alias: String = "default_expo_signature_alias"
-    @Field val size: Int = 256
-}
+data class KeySpec(
+    @Field val algorithm: SignatureAlgorithm = SignatureAlgorithm.EC,
+    @Field val alias: String = "default_expo_signature_alias",
+    @Field val size: Int = 256,
+): Record
 
